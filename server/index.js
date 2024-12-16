e788d8589726a369a6e5eb9a734d148bb7c78e01
jesus.emiliano123@hotmail.com <jesus.emiliano123@hotmail.com>
2024-12-15 21:10:48 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import logger from 'morgan';
+import { Server } from 'socket.io';
+import {createServer} from 'node:http';
+
+const port = process.env.PORT ?? 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+app.use(logger('dev'))
+
+io.on('connection', (socket) => {
+    console.log('A user has connected!')
+
+    socket.on('disconnect', () =>{
+        console.log('An user has disconnected')
+    })
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html');
+})
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+}) 
